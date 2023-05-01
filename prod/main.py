7cfda6f4e3fa665fcff59ac6f02973845a4c811b
yaq1n0 <yaqinhasan@gmail.com>
2023-05-02 00:39:41 +0100

    change based

--- prod/main.py
+++ prod/main.py
@@ -44,7 +44,7 @@ class ThymPi:
         self.test_duration = 2  # compliance test duration (seconds, default=2)
         self.test_speed = 500  # compliance test speed (default=500)
         self.frame_size = 50  # accelerometer reading frame size (default=50)
-        
+
         # call setup methods
         self.setupModel()
         self.setupThymio()
@@ -130,7 +130,7 @@ class ThymPi:
         i = 0
 
         end = time.time() + self.test_duration
-        
+
         while (time.time() <= end):
             xs.append(self.sensor.get_accel_data()["x"])
             i += 1
@@ -158,7 +158,67 @@ class ThymPi:
         if self.verbose:
             print("known compliances: ")
             for comp in self.compliances:
-                if self.compliances[comp] != None: 
+                if self.compliances[comp] != None:
+                    print("class: {} | compliance: {}".format(comp, self.compliances[comp]))
+
+    def testCompliance(self, class_name):
+        # perform calibration at start (stop, wait and calibrate)
+        self.setSpeed(0, 0)
+        time.sleep(0.2)
+
+        c_mean, c_error = self.calibrateSensor()
+        noise_floor = round(c_error - c_mean, 2)
+
+        self.setSpeed(self.test_speed, self.test_speed)
+
+        xs = []
+        accel_event = []
+        decel_event = []
+
+        accel_events = []
+        decel_events = []
+        i = 0
+        prev = 0
+
+        end = time.time() + self.test_duration
+
+        while (time.time() <= end):
+            i += 1
+            x = self.sensor.get_accel_data()["x"] - c_mean
+            xs.append(x)
+
+            # only perform calculations every time an accel_event or decel_event occurs\
+            if x > noise_floor:
+                if prev > 0:
+                    # accel -> accel
+                    accel_event.append(x)
+                elif prev <= 0:
+                    # decel -> accel
+                    decel_events.append(sum(decel_event) / len(decel_event))
+                    if self.verbose:
+                        print("end decel event: " + str(decel_events[-1]))
+
+            elif x < -1 * noise_floor:
+                if prev > 0:
+                    # accel -> decel
+                    accel_events.append(sum(accel_event) / len(accel_event))
+                    if self.verbose:
+                        print("end accel event: " + str(accel_events[-1]))
+                elif prev <= 0:
+                    # decel -> decel
+                    decel_event.append(x)
+
+            prev = x
+
+        self.goBackCM(10)
+
+        compliance = self.getCompliance(decel_events)
+        self.compliances[class_name] = compliance
+
+        if self.verbose:
+            print("known compliances: ")
+            for comp in self.compliances:
+                if self.compliances[comp] != None:
                     print("class: {} | compliance: {}".format(comp, self.compliances[comp]))
 
     def getCompliance(self, decel_events):
@@ -188,17 +248,17 @@ class ThymPi:
                                                 )
 
         objects = {}
-        
-        if len(classIds) > 0: 
+
+        if len(classIds) > 0:
             obj_names = []
             obj_confidences = []
 
             for classId in classIds:
                 obj_names.append(self.class_names[classId - 1])
-                
+
             for conf in confs:
                 obj_confidences.append(round(conf * 100, 0))
-                
+
             objects = dict(zip(obj_names, obj_confidences))
 
             if self.verbose:
@@ -208,17 +268,17 @@ class ThymPi:
 
     def setSpeed(self, left, right):
         self.aseba_network.SendEventName('motor.target', [left, right])
-        
-    def goBackCM(self, distance_cm, speed=250): 
+
+    def goBackCM(self, distance_cm, speed=250):
         # go back x centimeters
         # 500 motor speed is roughly ~20cm/s
         # default speed=250 (~10cm/s)
-        
+
         duration = round(distance_cm / (speed / 25), 0)
         thympi.setSpeed(-1 * speed, -1 * speed)
         time.sleep(duration)
-        thympi.setSpeed(0,0)
-        
+        thympi.setSpeed(0, 0)
+
 
 if __name__ == '__main__':
     # create thympi object
@@ -229,17 +289,17 @@ if __name__ == '__main__':
     cap.set(3, 640)
     cap.set(4, 480)
 
-    while True:   
+    while True:
         if thympi.aseba_network.GetVariable('thymio-II', 'prox.horizontal')[2] > 0:
             # if center prox detects something, (detection distance ~ 10cm)
-            if thympi.verbose: 
+            if thympi.verbose:
                 print("object in proximity!")
 
             objects = []
-            
+
             # go back 10cm (20cm tends to detect most objects)
             thympi.goBackCM(10)
-            
+
             thympi.confidence_threshold = 0.5
             while len(objects) == 0:
                 if thympi.verbose:
@@ -250,9 +310,9 @@ if __name__ == '__main__':
 
             for object in objects:
                 print("{} detected with {}% confidence".format(object, objects[object]))
-            
+
             maxConfObject = max(objects, key=objects.get)
-            
+
             if thympi.compliances[maxConfObject] != None:
                 print("compliance of {} is known to be {}".format(maxConfObject, thympi.compliances[maxConfObject]))
                 tmp = input("do you want to retest the compliance of {}?: ".format(maxConfObject))
